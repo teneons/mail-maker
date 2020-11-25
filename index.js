79b02c3aea6510e6337f363012d68daa0f88a26a
teneons <teneons@gmail.com>
2020-11-25 13:05:13 +0200

    made base ui processing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const hbsExpress = require('express-handlebars');
 
 
 //server base
@@ -8,5 +9,23 @@ app.listen(port, () => {
   console.log(`Server started on port - ${port}`)
 })
 
+//handlebars
+const hbs = hbsExpress.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
 
+app.engine('hbs', hbs.engine) //declaration hbs
+app.set('view engine', 'hbs') //use hbs
+app.set('views', 'views') //set hi
+
+
+app.get('/', (req, res) => {
+  res.status(200)
+
+  res.render('user', {
+    title: 'Add new user',
+    isUser: true
+  })
+})
 
